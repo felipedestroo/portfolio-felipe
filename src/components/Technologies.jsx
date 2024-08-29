32175503045a8bc4c7bba934addb45f53ec7b9a8
felipedestroo <fdestrocontact@gmail.com>
2024-08-29 16:43:22 -0300

    feat: add framer-motion

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,35 +5,86 @@ import { SiTypescript } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { SiNodedotjs } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
+import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+    initial: { y: -4.5 },
+    animate: {
+        y: [4.5, -4.5],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        }
+    }
+})
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h1 className="my-20 text-center text-4xl">Tech Stacks:</h1>
-        <div className="flex flex-wrap items-start justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.h1
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.5 }} 
+            className="my-20 text-center text-4xl">
+                Tech Stacks:
+        </motion.h1>
+        <motion.div 
+            whileInView={{ opacity: 1, x: 0 }} 
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1.5 }}
+            className="flex flex-wrap items-start justify-center gap-4">
+            <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsFill className="text-5xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(2.2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <TbBrandNextjs className="text-5xl"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(2.4)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiJavascript className="text-5xl text-yellow-300"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(2.6)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiTypescript className="text-5xl text-blue-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(2.8)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiTailwindcss className="text-5xl text-cyan-300"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3.0)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiNodedotjs className="text-5xl text-green-500"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+                variants={iconVariants(3.2)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiMysql className="text-5xl"/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
