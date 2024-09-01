36d7c2d9afa1c8428c2542999dab6bfa24c93499
felipedestroo <fdestrocontact@gmail.com>
2024-09-01 09:47:27 -0300

    feat: react-icons

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,10 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+// import { FiGithub } from "react-icons/fi";
+// import { FiExternalLink } from "react-icons/fi";
+
+
 
 export const HERO_CONTENT = `Eu sou um entusiasta da programação, aprendendo a construir aplicações web robustas e responsíveis. Desenvolvendo desde 2022, venho aperfeiçocando minhas habilidades com front-end e suas tecnologias 
 como React, Next.js e TypeScript, bem como tecnologias de back-end e banco de dados, como Node.js e MySQL. 
@@ -16,6 +20,8 @@ export const PROJECTS = [
     description:
       "Aplicação desenvolvida para simular um feed de rede social, introduzindo interatividades como comentar, curtir e excluir comentários e proporcionando uma experiência de usuária fluida e agradável",
     technologies: ["HTML", "CSS", "React", "TypeScript"],
+    // repository: <FiGithub />,
+    // deploy: <FiExternalLink />,
   },
   {
     title: "Portfolio Website",
@@ -23,6 +29,8 @@ export const PROJECTS = [
     description:
       "Site de portfólio pessoal que exibe projetos, habilidades e informações de contato, ideal para destacar competências e facilitar conexões profissionais.",
     technologies: ["React", "Tailwind CSS"],
+    // repository: <FiGithub />,
+    // deploy: <FiExternalLink />,
   },
   {
     title: "Megazord",
@@ -30,6 +38,8 @@ export const PROJECTS = [
     description:
       "Aplicativo Web que hospeda minijogos como xadrez, quebra-cabeça, jogo da velha e quadro branco, proporcionando uma plataforma digital interativa para os usuários se divertirem.",
     technologies: ["Next.js", "Tailwind CSS", "TypeScript"],
+    // repository: <FiGithub />,
+    // deploy: <FiExternalLink />,
   },
   {
     title: "HosToday",
@@ -37,6 +47,8 @@ export const PROJECTS = [
     description:
       "Plataforma de hospedagem de domínios com ofertas personalizadas e função de busca, facilitando a aquisição e gerenciamento de domínios de forma eficiente.",
     technologies: ["HTML", "CSS"],
+    // repository: <FiGithub />,
+    // deploy: <FiExternalLink />,
   },
 ];
 
