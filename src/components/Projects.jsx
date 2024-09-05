d66dcf33100f439e64a4cb453f632d4362a9d301
felipedestroo <fdestrocontact@gmail.com>
2024-09-05 16:34:18 -0300

    feat: start card box

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,42 +16,25 @@ function Projects() {
                 className="my-20 text-center text-4xl">
                     Projetos
             </motion.h2>
-            <div className="flex pb-4 space-x-6">
-                <div className="w-1/3 h-96 bg-black border rounded-md py-2 px-4">
-                    <div className="flex flex-col">
-                        <h3 className="text-green-500 font-bold py-2">Ignite Feed</h3>
-                        <p>Projeto desenvolvido para simular uma rede social com funções de likes e comentários</p>
-                    </div>
+            <div className="flex justify-center w-auto m-8">
+              <div className="bg-black border border-neutral-800 w-96 h-96 rounded-md">
+                <div className="p-8">
+                  <h3 className="text-2xl mb-2">Ignite Feed</h3>
+                  <p>Aplicação desenvolvida para simular um feed de rede social, introduzindo interatividades</p>
                 </div>
-                <div className="w-1/3 h-96 bg-black border rounded-md py-2 px-4">
-                    <div className="flex flex-col">
-                        <h3 className="text-orange-500 font-bold py-2">Portfolio</h3>
-                        <p>Portfolio pessoal para divulgar meus projetos e conteúdos</p>
-                    </div>
+                <div className="flex justify-center">
+                  <a href="#" className="flex items-center hover:bg-neutral-800 rounded-md">
+                    <button className="text-2xl">
+                      <FiExternalLink />
+                    </button>
+                  </a>
+                  <a href="#" className="flex items-center hover:bg-neutral-800 rounded-md">
+                    <button className="text-2xl">
+                      <FiGithub />
+                    </button>
+                  </a>
                 </div>
-                <div className="w-1/3 h-96 bg-black border rounded-md py-2 px-4">
-                    <div className="flex flex-col">
-                        <h3 className="text-blue-500 font-bold py-2">Megazord</h3>
-                        <p>Aplicativo Web que hospeda minijogos como xadrez, quebra-cabeça, jogo da velha e jogo da memória.</p>
-                    </div>
-                    <div className="flex justify-between bottom">
-                        <button className="border rounded-md w-auto p-2">
-                            Saiba mais
-                        </button>
-                        <div className="flex">
-                            <a href="#" className="flex hover:bg-neutral-800 w-8 h-8 rounded-md text-lg">
-                            <button className="items-center justify-center m-auto">
-                                <FiExternalLink/>
-                            </button>
-                        </a>
-                        <a href="#" className="flex hover:bg-neutral-800 w-8 h-8 rounded-md text-lg">
-                            <button className="items-center justify-center m-auto">
-                                <FiGithub/>
-                            </button>
-                        </a>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
         </div>
     </section>
