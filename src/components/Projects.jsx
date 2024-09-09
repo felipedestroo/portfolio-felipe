7b326fcf78a44579f9ec04bf53d86ba35232e852
felipedestroo <fdestrocontact@gmail.com>
2024-09-09 20:04:03 -0300

    feat: update card box

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,15 +16,15 @@ function Projects() {
               className="my-20 text-center text-4xl">
               Projetos
           </motion.h2>
-          <div className="flex justify-center w-auto m-8">
-            <div className="bg-black border border-neutral-800 w-1/2 h-auto rounded-md p-6">
+          <div className="flex justify-center items-center text-wrap gap-8 lg:w-full m-8">
+            <div className="bg-black border border-neutral-800 w-1/2 rounded-md p-6">
               <h3 className="text-2xl mb-2">Ignite Feed</h3>
               <p className="">
                 Aplicação desenvolvida para simular um feed de rede social, introduzindo interatividades como comentar, 
                 curtir e excluir comentários e proporcionando uma experiência de usuária fluida e agradável.
               </p>
               <div className="flex">
-                <img src={project1} alt="Foto do Projeto Ignite Feed" className="border border-neutral-800 rounded-md mt-4 mb-6"/>
+                <img src={project1} alt="Foto do Projeto Ignite Feed" className="border border-neutral-800 rounded-md mt-4 mb-6 w-full"/>
               </div>
               <div className="flex justify-center gap-6">
                 <a href="https://ignite-feed-felipedestrooo.vercel.app/" target="_blank" className="flex items-center hover:text-orange-500 rounded-md">
@@ -39,9 +39,9 @@ function Projects() {
                 </a>
               </div>
             </div>
-            <div className="bg-black border border-neutral-800 w-1/2 h-auto rounded-md p-6">
+            <div className="bg-black border border-neutral-800 w-1/2 rounded-md p-6">
               <h3 className="text-2xl mb-2">Personal Portfolio</h3>
-              <p className="">
+              <p>
                 Site de portfólio pessoal que exibe projetos, habilidades e informações de contato, ideal para 
                 destacar competências e facilitar conexões profissionais.
               </p>
