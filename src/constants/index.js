e33e3a05bc3a5e4b1a08a5b1f3f7a08268d84118
felipedestroo <fdestrocontact@gmail.com>
2024-09-10 10:18:55 -0300

    feat: update prompts

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,3 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
-// import { FiGithub } from "react-icons/fi";
-// import { FiExternalLink } from "react-icons/fi";
-
-
-
 export const HERO_CONTENT = `Eu sou um entusiasta da programação, aprendendo a construir aplicações web robustas e responsíveis. Desenvolvendo desde 2020, venho aperfeiçoando minhas habilidades com front-end e suas tecnologias 
 como React, Next.js e TypeScript, bem como tecnologias de back-end e banco de dados, como Node.js e MySQL. 
 Meu objetivo é aproveitar minha experiência para criar soluções inovadoras que impulsionem o crescimento dos negócios e proporcionem experiências excepcionais ao usuário.`;
@@ -14,45 +5,6 @@ Meu objetivo é aproveitar minha experiência para criar soluções inovadoras q
 export const ABOUT_TEXT = `Olá, sou o Felipe Destro, engenheiro de software brasileiro e atualmente cursando o sexto semestre de Bacharelado em Ciência da Computação. Minha jornada na programação começou em 2020,
 quando, ao desenvolver meu primeiro TCC utilizando JavaScript, percebi a força transformadora da tecnologia. Na época, também fui programador na equipe de robótica da
  FRC (First Robotics Competition), o que reforçou ainda mais minha paixão pela área. Com o tempo, me especializei em front-end, onde encontro satisfação em criar experiências interativas e intuitivas.`;
-
-export const PROJECTS = [
-  {
-    title: "Ignite Feed",
-    image: project1,
-    description:
-      "Aplicação desenvolvida para simular um feed de rede social, introduzindo interatividades como comentar, curtir e excluir comentários e proporcionando uma experiência de usuária fluida e agradável",
-    technologies: ["HTML", "CSS", "React", "TypeScript"],
-    // repository: <FiGithub />,
-    // deploy: <FiExternalLink />,
-  },
-  {
-    title: "Portfolio Website",
-    image: project2,
-    description:
-      "Site de portfólio pessoal que exibe projetos, habilidades e informações de contato, ideal para destacar competências e facilitar conexões profissionais.",
-    technologies: ["React", "Tailwind CSS"],
-    // repository: <FiGithub />,
-    // deploy: <FiExternalLink />,
-  },
-  {
-    title: "Megazord",
-    image: project3,
-    description:
-      "Aplicativo Web que hospeda minijogos como xadrez, quebra-cabeça, jogo da velha e quadro branco, proporcionando uma plataforma digital interativa para os usuários se divertirem.",
-    technologies: ["Next.js", "Tailwind CSS", "TypeScript"],
-    // repository: <FiGithub />,
-    // deploy: <FiExternalLink />,
-  },
-  {
-    title: "HosToday",
-    image: project4,
-    description:
-      "Plataforma de hospedagem de domínios com ofertas personalizadas e função de busca, facilitando a aquisição e gerenciamento de domínios de forma eficiente.",
-    technologies: ["HTML", "CSS"],
-    // repository: <FiGithub />,
-    // deploy: <FiExternalLink />,
-  },
-];
 
 export const CONTACT = {
   address: "São Paulo, Brazil ",
