37c84ce6a0d8ea307bac6e3e8cf55536d32ada6d
felipedestroo <fdestrocontact@gmail.com>
2024-11-12 19:03:25 -0300

    feat: add email function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,35 +1,136 @@
-import { motion } from "framer-motion"
+// import { motion } from "framer-motion"
 import { useTranslation } from "react-i18next"
+import emailjs from '@emailjs/browser';
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { FiSend } from "react-icons/fi";
+import { Toaster } from "react-hot-toast";
 
 function Contact() {
 
   const { t } = useTranslation();
 
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+
+  const [errors,setErrors] = useState({});
+  const [isSending, setIsSending] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const validate = () => {
+    let errors = {};
+    if (!formData.name) errors.name = t("error.errorName")
+    if (!formData.email) {
+      errors.email = t("error.errorEmail");
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = t("error.errorEmailInvalid");
+    }
+    if (!formData.message) errors.message = t("error.errorMessage");
+    return errors;
+  }
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  const validationErrors = validate();
+  if (Object.keys(validationErrors).length > 0) {
+    setErrors(validationErrors);
+  } else {
+    setErrors({});
+    setIsSending(true);
+
+      emailjs
+        .send(
+          "service_v5k3pvf",
+          "template_gwrlaqb",
+          formData,
+          "VLBp9X9VDKmIkpc3-"
+        )
+        .then(() => {
+          toast.success(t("toast.success"));
+          setFormData({ name: "", email: "", message: "" });
+        })
+        .catch((error) => {
+          console.log(t("toast.console"), error);
+          toast.error(t("toast.error"));
+        })
+        .finally(() => {
+          setIsSending(false);
+        });
+    } 
+  }
+
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <motion.h2 
-          whileInView={{opacity: 1, y: 0}}
-          initial={{opacity: 0, y: -100}}
-          transition={{duration: 0.5}}
-          viewport={{ once: true }}
-          className="my-10 text-center text-4xl">{t("contact.title")}</motion.h2>
-        <div className="text-center tracking-tighter">
-            <motion.p 
-              whileInView={{opacity: 1, x: 0}}
-              initial={{opacity: 0, x: -100}}
-              transition={{duration: 1}}
-              viewport={{ once: true }}
-              className="my-4">{t("contact.address")}</motion.p>
-            <motion.p 
-              whileInView={{opacity: 1, x: 0}}
-              initial={{opacity: 0, x: 100}}
-              transition={{duration: 1}}
-              viewport={{ once: true }}
-              className="my-4">{t("contact.phoneNo")}</motion.p>
-            <a href="mailto:felipedestrodev@gmail.com" className="border-b">
-                {t("contact.email")}
-            </a>
+      <Toaster />
+      <h2 className="my-8 text-center text-4xl tracking-tighter">{t("contact.title")}</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4 flex space-x-4">
+          <div className="lg:w-full">
+            <input type="text" 
+              id="name"
+              name="name"
+              value={formData.name}
+              placeholder={t("contact.nameSection")}
+              onChange={handleChange}
+              className="mb-2 w-full appearance-none rounded-lg border border-orange-300 px-3 py-2 text-sm
+              focus:border-orange-500 focus:outline-none bg-transparent text-white"
+            />
+            {errors.name && (
+              <p className="text-sm text-red-500">{errors.name}</p>
+            )}
+          </div>
+          <div className="lg:w-full">
+            <input type="text" 
+              id="email"
+              name="email"
+              value={formData.email}
+              placeholder={t("contact.emailSection")}
+              onChange={handleChange}
+              className="mb-2 w-full appearance-none rounded-lg border border-orange-300 px-3 py-2 text-sm
+              focus:border-orange-500 focus:outline-none bg-transparent text-white"
+            />
+            {errors.email && (
+              <p className="text-sm text-red-500">{errors.email}</p>
+            )}
+          </div>
         </div>
+        <div className="mb-8">
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              placeholder={t("contact.messageSection")}
+              onChange={handleChange}
+              className="mb-2 w-full appearance-none rounded-lg border border-orange-300 px-3 py-2 text-sm
+              focus:border-orange-500 focus:outline-none bg-transparent text-white"
+              rows="6"
+            />
+            {errors.message && (
+              <p className="text-sm text-red-500">{errors.message}</p>
+            )}
+          </div>
+          <button type="submit" className={`mb-8 w-full rounded border border-orange-500 bg-orange-500
+            px-4 py-2 text-sm font-semibold text-white hover:bg-orange-500 ${
+              isSending ? "cursor-not-allowed opacity-50" : ""
+            }`}
+            disabled={isSending}
+          >
+            <div className="flex items-center justify-center gap-2">
+              {isSending ? (t("contact.sendingButton")) : (t("contact.sendButton"))}
+              <FiSend />
+            </div>
+          </button>
+      </form>
     </div>
   )
 }
