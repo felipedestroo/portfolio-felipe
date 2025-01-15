4ded994dc38ecd0070d8e96c55f62c8085a8bf8f
felipedestroo <fdestrocontact@gmail.com>
2025-01-15 10:11:49 -0300

    feat: add radix ui dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { FaLinkedin, FaGithub } from "react-icons/fa";
@@ -6,18 +6,17 @@ import { RiGlobalLine } from "react-icons/ri";
 import { IoMdCheckmark } from 'react-icons/io';
 import logo from "../assets/FdLogoWhite.png";
 import i18n from 'i18next';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 const LanguageItem = ({ language, onClick, isSelected }) => {
   return (
-    <li
-      className={`px-4 py-2 hover:bg-neutral-800 cursor-default transition rounded-md ${isSelected ? 'selected' : ''}`}
+    <DropdownMenu.Item
+      className={`px-3 py-2 hover:bg-neutral-800 cursor-default transition rounded-md flex items-center justify-between w-32 text-base font-medium outline-none`}
       onClick={onClick}
     >
-      <div className="flex flex-shrink-0 items-center w-28">
-        {language}
-        {isSelected && <IoMdCheckmark className="ml-2" />}
-      </div>
-    </li>
+      {language}
+      {isSelected && <IoMdCheckmark className="ml-2 text-lg" />}
+    </DropdownMenu.Item>
   );
 };
 
@@ -28,34 +27,13 @@ LanguageItem.propTypes = {
 };
 
 const Navbar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState('en');
   const { t } = useTranslation();
-  const dropdownRef = useRef(null);
-
-  const toggleDropdown = () => {
-    setDropdownOpen((prev) => !prev);
-  };
 
   const handleChangeLanguage = (language) => {
     setSelectedLanguage(language);
     i18n.changeLanguage(language);
-    setDropdownOpen(false); // Close dropdown after selecting language
   };
-
-  useEffect(() => {
-    const handleOutsideClick = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleOutsideClick);
-
-    return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
-    };
-  }, []);
 
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
@@ -77,30 +55,27 @@ const Navbar = () => {
         >
           <FaGithub />
         </a>
-        <div className="relative" ref={dropdownRef}>
-          <button
-            className="hover:bg-neutral-800 w-10 h-10 flex justify-center items-center rounded-md transition"
-            onClick={toggleDropdown}
-          >
-            <RiGlobalLine />
-          </button>
-          {dropdownOpen && (
-            <div className="absolute right-0 mt-2 w-26 bg-neutral-900 rounded-md shadow-lg z-10">
-              <ul className="py-1 px-1 text-white text-lg">
-                <LanguageItem
-                  language={t('English')}
-                  onClick={() => handleChangeLanguage('en')}
-                  isSelected={selectedLanguage === 'en'}
-                />
-                <LanguageItem
-                  language={t('Português')}
-                  onClick={() => handleChangeLanguage('pt')}
-                  isSelected={selectedLanguage === 'pt'}
-                />
-              </ul>
-            </div>
-          )}
-        </div>
+
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button className="hover:bg-neutral-800 w-10 h-10 flex justify-center items-center rounded-md transition">
+              <RiGlobalLine />
+            </button>
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Content className="absolute right-0 mt-2 w-32 bg-neutral-900 rounded-md shadow-lg z-10">
+              <LanguageItem
+                language={t('English')}
+                onClick={() => handleChangeLanguage('en')}
+                isSelected={selectedLanguage === 'en'}
+              />
+              <LanguageItem
+                language={t('Português')}
+                onClick={() => handleChangeLanguage('pt')}
+                isSelected={selectedLanguage === 'pt'}
+              />
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       </div>
     </nav>
   );
